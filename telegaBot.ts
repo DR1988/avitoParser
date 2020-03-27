f92edb9390c6e0e8555dd0c333fbe2eb67290193
ARTGROUP\drakhmanov <Damir.Rakhmanov@artezio.com>
2020-03-27 23:03:48 +0400

    added bot

---
+++ telegaBot.ts
@@ -0,0 +1,27 @@
+import * as TelegramBot from 'node-telegram-bot-api'
+
+var token = '1079216795:AAGzdMS1W866f1saVCuFmKptmnarpVfkYwI';
+// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
+// Подробнее: https://core.telegram.org/bots/api#getupdates
+var bot = new TelegramBot(token, { polling: true });
+
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id; // Получаем ID отправителя
+    var resp = match[1]; // Получаем текст после /echo
+    bot.sendMessage(fromId, resp);
+});
+
+// Простая команда без параметров
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
+    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
+    var photo = 'cats.png'; // в папке с ботом должен быть файл "cats.png"
+    bot.sendPhoto(chatId, photo, { caption: 'Милые котята' });
+});
+const notes = []
+
+setInterval(() => {
+  bot.sendMessage(460803187, 'Напоминалка')
+}, 5000);
+// bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.')
